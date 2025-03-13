c75c21612bc0fc2e882a0bf293fdf1c7839f9afd
Guillermo Molina <guimocor@gmail.com>
2025-03-13 10:35:25 +0100

    creacion del ACR

---
+++ terraform/acr.tf
@@ -0,0 +1,13 @@
+# Configuración del ACR
+resource "azurerm_resource_group" "example" {
+  name     = "example-resources"
+  location = "West Europe"
+}
+
+resource "azurerm_container_registry" "acr" {
+  name                = "containerRegistryCP2"
+  resource_group_name = azurerm_resource_group.example.name
+  location            = azurerm_resource_group.example.location
+  sku                 = "Standard"
+  admin_enabled       = true
+}
